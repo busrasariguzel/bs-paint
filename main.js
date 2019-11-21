c4d3c29e34aa56c737a46268ce2951f4390299dd
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-11-21 11:49:07 -0500

    created a general function to change colors and added more colors

--- main.js
+++ main.js
@@ -6,39 +6,56 @@ function resetEverything() {
     document.location.reload(true);
 }
 
-document.querySelector('.color-1').addEventListener('click', changeBrush)
-function changeBrush() {
-    classes.replace(classes[1] , 'color-1')
-}
-
-document.querySelector('.color-2').addEventListener('click', changeBrush1)
-function changeBrush1() {
-    
-    classes.replace(classes[1], 'color-2')
-}
 
 
-document.querySelector('.color-3').addEventListener('click', changeBrush2)
-function changeBrush2() {
+const littleColors = document.querySelectorAll('.palette-color')
+let littleColorsArray = Array.from(littleColors)
+console.log(littleColorsArray.classList)
+const littleColorsArrayList = littleColorsArray.classList
+
+
+
+for( const eachColor of littleColorsArray){
     
-    classes.replace(classes[1], 'color-3')
-}
-document.querySelector('.color-4').addEventListener('click', changeBrush3)
-function changeBrush3() {
-    
-    classes.replace(classes[1], 'color-4')
+    eachColor.addEventListener('click', function(event) {
+
+    classes.replace(classes[1] ,eachColor.classList[1] )
+})
 }
 
-document.querySelector('.color-5').addEventListener('click', changeBrush4)
-function changeBrush4() {
+// document.querySelector('.color-1').addEventListener('click', changeBrush)
+// function changeBrush() {
+//     classes.replace(classes[1] , 'color-1')
+// }
+
+// document.querySelector('.color-2').addEventListener('click', changeBrush1)
+// function changeBrush1() {
     
-    classes.replace(classes[1], 'color-5')
-}
-document.querySelector('.color-6').addEventListener('click', changeBrush5)
-function changeBrush5() {
+//     classes.replace(classes[1], 'color-2')
+// }
+
+
+// document.querySelector('.color-3').addEventListener('click', changeBrush2)
+// function changeBrush2() {
     
-    classes.replace(classes[1], 'color-6')
-}
+//     classes.replace(classes[1], 'color-3')
+// }
+// document.querySelector('.color-4').addEventListener('click', changeBrush3)
+// function changeBrush3() {
+    
+//     classes.replace(classes[1], 'color-4')
+// }
+
+// document.querySelector('.color-5').addEventListener('click', changeBrush4)
+// function changeBrush4() {
+    
+//     classes.replace(classes[1], 'color-5')
+// }
+// document.querySelector('.color-6').addEventListener('click', changeBrush5)
+// function changeBrush5() {
+    
+//     classes.replace(classes[1], 'color-6')
+// }
 // const square = document.querySelector('.square')
 // const classesSquare = square.classList;
 
